011ea3114e16a6c860babf64b20c7d51a1238749
Tobias S. Sørensen <Tss3331@hotmail.com>
2017-09-05 10:07:32 +0200

    Opretted server

---
+++ MiniChatServer/Server.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace MiniChatServer
+{
+	public class Server
+	{
+		
+		public int IP { get; set; }
+
+		//Constructor
+		public Server()
+		{
+
+		}
+
+		//Method
+		public void Start()
+		{
+			TcpListener TCPL = new TcpListener(IPAddress.Loopback, 7);
+			Start();
+
+			while (true)
+			{
+				using (TcpClient socket = TCPL.AcceptTcpClient())
+				using (NetworkStream ns = socket.GetStream())
+				using (StreamReader sr = new StreamReader(ns))
+				using (StreamWriter sw = new StreamWriter(ns))
+				{
+					string line = sr.ReadLine();
+					Console.WriteLine(line);
+					
+					string myLine = Console.ReadLine();
+					sw.WriteLine(myLine);
+
+					Console.WriteLine("Client: " + line );
+					
+					sw.Flush();
+
+					if (line == "STOP" || myLine == "STOP")
+					{
+						sw.WriteLine();
+						sw.Flush();
+					}
+				}
+			}
+		}
+	}
+}
